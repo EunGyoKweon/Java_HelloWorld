dd5964ac836b6f77c76c77dcd0874c2d6e26ebc3
EunGyoKweon <22000042@handong.edu>
2021-09-03 13:29:06 +0900

    fourth commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -11,7 +11,7 @@ public class Main {
       System.out.print("나이 입력 : ");
       int age = ss.nextInt();
       System.out.println(age + "살 " + "[" + name + "]님 환영합니다~");
-      System.out.println("실전프로젝트 1주차 실습과제");
+      System.out.println("실전프로젝트 1주차 실습과제 - 22000042 권은교");
 
    }
 }
