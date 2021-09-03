12a7d9eea7e3f190ba5779f7ffbe5e543635f46b
EunGyoKweon <22000042@handong.edu>
2021-09-03 12:44:46 +0900

    second commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
       Scanner s = new Scanner(System.in);
+      Scanner ss = new Scanner(System.in);
       System.out.println("Hello World!!!\n");
       System.out.print("이름 입력 : ");
       String name = s.next();
-      System.out.println("[" + name + "]님 환영합니다~");
+      System.out.print("나이 입력 : ");
+      int age = ss.nextInt();
+      System.out.println(age + "살 " + "[" + name + "]님 환영합니다~");
+      
+      
       
    }
 }
